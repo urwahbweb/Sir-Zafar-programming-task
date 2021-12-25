ee8b68c87cd61e25fc8123d8c06d0db8756e00df
urwahbweb <94514870+urwahbweb@users.noreply.github.com>
2021-12-25 11:11:50 -0800

    Add files via upload

---
+++ task1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2 As String
+        Dim isPangram As Boolean
+        Dim Str1Char, Str2Char As Char
+
+        Console.ReadLine()
+        Str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        Str2 = UCase("A quick brown fox jumps over the lazy dog")
+
+        Console.WriteLine("Enter Str2: ")
+
+
+        If isPangram = True Then
+
+            Console.WriteLine("Str2 is Pangram")
+        Else
+            Console.WriteLine("Str2 is Not Pangram")
+
+
+        End If
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
---
+++ task6.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim donutcount As Integer
+        donutcount = 0
+
+        Console.WriteLine("Enter the number of donuts: ")
+        donutcount = Console.ReadLine()
+
+        If donutcount < 10 Then
+            Console.WriteLine("number of donuts: ")
+            Console.WriteLine(donutcount)
+        ElseIf donutcount >= 10 Then
+            Console.WriteLine("number of donuts: many")
+        End If
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
